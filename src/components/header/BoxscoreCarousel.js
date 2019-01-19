cea1767e4fd5c0f3b1162f37b59c8e89626d2f9c
arashbabaie6 <arash.babaie6@gmail.com>
2019-01-19 21:53:12 +0330

    make persian boxscore

--- src/components/header/BoxscoreCarousel.js
+++ src/components/header/BoxscoreCarousel.js
@@ -23,6 +23,7 @@ class BoxscoreCarousel extends Component {
     });
   }
   decDay = () => {
+    this.setState({ scoreboardDate: "" });
     axios
       .get(
         `http://localhost:5000/scoreboard/decDay/${this.state.scoreboardDate}`
@@ -35,6 +36,7 @@ class BoxscoreCarousel extends Component {
       });
   };
   incDay = () => {
+    this.setState({ scoreboardDate: "" });
     axios
       .get(
         `http://localhost:5000/scoreboard/incDay/${this.state.scoreboardDate}`
@@ -51,29 +53,29 @@ class BoxscoreCarousel extends Component {
     var settings = {
       infinite: false,
       speed: 500,
-      slidesToShow: 5,
-      slidesToScroll: 5,
+      slidesToShow: 4,
+      slidesToScroll: 2,
       initialSlide: 0,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4
+            slidesToShow: 3,
+            slidesToScroll: 2
           }
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToShow: 2,
+            slidesToScroll: 2
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1
           }
         }
       ]
--- src/components/header/SingleBoxscore.js
+++ src/components/header/SingleBoxscore.js
@@ -1,16 +1,47 @@
 import React from "react";
+import ORL from "./../../../assets/NBA teams logo/ORL.svg";
 
 export default function SingleBoxscore(props) {
   return (
-    <div>
-      <div className="BoxscoreItem Home" id="test">
-        <div id="backImage" />
-        <p>{props.gameInfo.home.abbreviation}</p>
-        <p className="BoxscoreItemScoreHome">{props.gameInfo.home.score}</p>
+    <div className="BoxscoreItemContainer">
+      <div className="BoxscoreItem Home">
+        <div className="BoxscoreItemPic">
+          <img src={ORL} />
+        </div>
+        <div className="BoxscoreItemName">
+          <h4>
+            {props.gameInfo.home.name_persian.city}{" "}
+            {props.gameInfo.home.name_persian.title}
+          </h4>
+        </div>
+        <div className="BoxscoreItemScore">
+          <h4>{props.gameInfo.home.score}</h4>
+        </div>
       </div>
-      <div className="BoxscoreItem Away" id="test2">
-        <p className="BoxscoreItemScoreAway">{props.gameInfo.visitor.score}</p>
-        <p>{props.gameInfo.visitor.abbreviation}</p>
+      <div className="BoxscoreItem Away">
+        <div className="BoxscoreItemPic">
+          <img src={ORL} />
+        </div>
+        <div className="BoxscoreItemName">
+          <h4 >
+            {" "}
+            {props.gameInfo.visitor.name_persian.city}{" "}
+            {props.gameInfo.visitor.name_persian.title}
+          </h4>
+        </div>
+        <div className="BoxscoreItemScore">
+          <h4>{props.gameInfo.visitor.score}</h4>
+        </div>
+      </div>
+      <div className="BoxscoreItem BoxscoreItemInfo">
+        {props.gameInfo.home.score == "" ? (
+          <React.Fragment>
+            <span>{props.gameInfo.date_ir}</span>
+            <span>{props.gameInfo.time_ir}</span>
+          </React.Fragment>
+        ) : (
+          <span className="BoxscoreItemInfomoreInfo">اطلاعات بیشتر</span>
+        )}
       </div>
     </div>
   );
